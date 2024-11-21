6ae5bc6f54647f923e61ccb5776278e8cdb299a7
Nicolas <nicolas.ale.girotti@gmail.com>
2024-11-21 14:34:53 -0300

    Sucesores

--- linkedlist-tp/main.cpp
+++ linkedlist-tp/main.cpp
@@ -15,6 +15,17 @@ int sumatoria(LinkedList xs){
 }
 
 
+//Incrementa en uno todos los elementos.
+void Sucesores(LinkedList xs){
+    ListIterator ixs = getIterator(xs);
+    while (!atEnd(ixs)) {
+        int valor = current(ixs) + 1;
+        SetCurrent(valor,ixs);
+        Next(ixs);
+    }
+    DisposeIterator(ixs);
+}
+
 
 int main(){
     LinkedList miLista = nil();
@@ -23,11 +34,12 @@ int main(){
     Cons(3, miLista);
     Cons(4, miLista);
     Tail(miLista);
-    Snoc(miLista,10);
+    Snoc(miLista,5);
 
 
     cout << "Mi lista " << endl;
     cout << "head -> " << head(miLista)<< endl;
     cout << "Esta vacia? -> " << isEmpty(miLista)<< endl; 
+    Sucesores(miLista);
     cout << "Sumatoria -> " << sumatoria(miLista)<< endl;
 }
